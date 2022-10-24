eee5af54fdf1f91140a235fb14bcb54986857d02
Michael Lass <michael.lass@uni-paderborn.de>
2022-10-24 09:35:14 +0200

    WIP: Enforce JDK18 for JabRef

--- jabref/jabref.sh
+++ jabref/jabref.sh
@@ -14,7 +14,7 @@
 
 ROOT=/usr/share/java/jabref
 
-/usr/bin/archlinux-java-run -a 18 -- \
+/usr/bin/archlinux-java-run -a 18 -b 18 -- \
 --add-modules javafx.controls,javafx.fxml,javafx.swing,javafx.web \
 --add-exports javafx.controls/com.sun.javafx.scene.control=org.controlsfx.controls \
 --add-exports org.controlsfx.controls/impl.org.controlsfx.skin=org.jabref \
